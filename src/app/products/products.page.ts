95f062b53c93d475a026e64539f2a35de7148298
Roel Vroom <r0950679@student.thomasmore.be>
2024-07-26 11:39:57 +0200

    css aangepast home, api call gemaakt in products page

--- src/app/products/products.page.ts
+++ src/app/products/products.page.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.page.scss', '../app.component.scss'],
 })
 export class ProductsPage implements OnInit {
+  products: any[] = []; // Voeg deze regel toe
 
   constructor() { }
 
   ngOnInit() {
+    this.fetchProducts();
   }
 
+  fetchProducts() {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(json => {
+        this.products = json; // Update deze regel
+        console.log(json);
+      })
+      .catch(error => console.error('Error fetching products:', error));
+  }
 }
