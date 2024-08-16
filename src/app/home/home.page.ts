e2fc21a52aeb07a22f7f6b54b535885acaa6c723
Roel Vroom <r0950679@student.thomasmore.be>
2024-08-16 12:26:56 +0200

    style home done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,8 +4,7 @@ import { ProductManagementService } from '../product-management-service.service'
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
-  //'../app.component.scss'
+  styleUrls: ['./home.page.scss', '../app.component.scss'],
 })
 export class HomePage implements OnInit {
 
